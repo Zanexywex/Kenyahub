53172b72128a9cb886adb3238a4962345a65db8f
Zanexywex <thanarat.wex@gmail.com>
2026-07-27 17:12:25 +0700

    Update KeynaHub.Loader.lua

--- KeynaHub.Loader.lua
+++ KeynaHub.Loader.lua
@@ -2,6 +2,8 @@ if game.GameId == 10144587520 then -- AnimeCardFarm
   loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/2807b2a93416e0a036e4fe65bdb558ff.lua"))()
 if game.GameId == 994732206 then -- BloxFeet
   loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/911fbd5ece03e371717724715ec3ff3b.lua"))()
-elseif game.GameId == 66654135 then
+elseif game.GameId == 66654135 then -- mm2
   loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/9637ca1874782c02de2c21e9f5b720c3.lua"))()
+elseif game.GameId == 4658598196 then -- aotr
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/1b3021d3beec89f76afd2847ffc72582.lua"))()
 end
